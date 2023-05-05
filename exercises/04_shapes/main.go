b6b25821a6fe745de11c0cb875be9d5d1862e4b4
Valeria Tisch <valeria.tisch97@gmail.com>
2023-05-05 13:11:11 +0200

    typo

--- exercises/04_shapes/main.go
+++ exercises/04_shapes/main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-TODO: Createa a shape area calculator
+TODO: Create a shape area calculator
 Define an interface called 'Shape' with a method 'Area() float64'.
 Implement the 'Shape' interface for different shapes (e.g., Circle, Rectangle, Triangle).
 Create a function 'CalculateTotalArea' that takes a slice of Shapes and returns the sum of their areas.
