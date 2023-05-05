37095f852d344cea064bb574d57e86da1c3065bb
Valeria Tisch <valeria.tisch97@gmail.com>
2023-05-05 12:33:31 +0200

    new example

--- 08_interfaces/main.go
+++ 08_interfaces/main.go
@@ -4,20 +4,10 @@ package main
 // An interface is a set of method signatures.
 // A type implements an interface by implementing its methods.
 // There is no explicit declaration of intent, no "implements" keyword.
-// Shape interface defines a single method, Area.
 
-// Rectangle and Circle types implement the Shape interface.
-type Rectangle struct {
-	Width, Height float64
-}
+// Human interface defines a single method, breath.
 
-type Circle struct {
-	Radius float64
-}
-
-func (c *Circle) increaseRadius() {
-	c.Radius++
-}
+// Adult and child types implement the Human interface.
 
 // Function that takes an interface as an argument
 
