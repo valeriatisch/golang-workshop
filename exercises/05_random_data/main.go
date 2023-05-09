2034c6a282af9a055338d10d5aaaafc2ef0bad9b
Valeria Tisch <valeria.tisch97@gmail.com>
2023-05-09 16:22:25 +0300

    random data context exercise solution

--- exercises/05_random_data/main.go
+++ exercises/05_random_data/main.go
@@ -1,21 +1,37 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
 // Random Data API Docs: https://random-data-api.com/documentation
 
 // TODO: Create constant for the base URL: https://random-data-api.com/api/v2/
+const baseURL = "https://random-data-api.com/api/v2/"
 
-// TODO: Create a struct for a resource you'd like to fetch
+// TODO: Create a struct any resource you'd like to fetch
 // TODO: Add json tags to the struct fields to unmarshal the response
-// For example:
+// For example
 type someResource struct {
 	ID int `json:"id"`
 }
 
-// TODO: Uncomment the following function when you're ready to use it.
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
+
 // fetchRandomData fetches random data of the specified size from the provided resource.
 // It takes a context for cancellation and timeout, the resource name, and the desired size.
 // The function returns the fetched data as a byte slice and any error encountered.
-/*
 func fetchRandomData(ctx context.Context, resource string, size int) ([]byte, error) {
 	// Create a new HTTP GET request for the resource
 	req, err := http.NewRequest("GET", baseURL+resource, nil)
@@ -41,24 +57,47 @@ func fetchRandomData(ctx context.Context, resource string, size int) ([]byte, er
 	// Read the response body and return it as a byte slice
 	return ioutil.ReadAll(resp.Body)
 }
-*/
 
 func main() {
-	// General TODO: Always handle errors
-	// TODO: Measure the total execution time of the program, hint: time.Now() & time.Since()
+	now := time.Now()
+	// Open a file for writing the fetched user data
+	file, err := os.Create("random_users.csv")
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return
+	}
+	defer file.Close()
 
-	// TODO: Create a CSV file, where you will write the fetched data, don't forget to close it, hint: os.Create()
+	// Write the fetched user data to the file
+	_, _ = file.WriteString("ID, Name, Email\n")
+	// General TODO: always handle errors
+	// TODO: Create a context with a timeout of 800 milliseconds
+	
+	n := 0
+	for n < 10 {
+		n++
 
-	// TODO: First line of the CSV file should be the column names, hint: file.WriteString()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
 
-	// TODO: Create a context with a reasonable timeout
+		// TODO: Fetch random data from the API with random size
+		data, err := fetchRandomData(ctx, "users", 100)
+		if err != nil {
+			fmt.Printf("Error fetching data: %v\n", err)
+			return
+		}
 
-	// TODO: Fetch at least 1000 records from the API using fetchRandomData(), but notice it can only return 100 records at a time
-	// 		you might want to put part of the code in a loop
-	// 		also, fetchRandomData() expects a context
+		// TODO: Unmarshal the response data into your struct with json.Unmarshal()
+		var users []User
+		if err := json.Unmarshal(data, &users); err != nil {
+			fmt.Printf("Error unmarshalling data: %v\n", err)
+			return
+		}
 
-	// TODO: Unmarshal the response data into a slice of your struct, hint: json.Unmarshal()
-
-	// TODO: Put the fetched data into a file, hint: fmt.Sprintf()
-
+		// Put data into a file
+		for _, user := range users {
+			_, _ = file.WriteString(fmt.Sprintf("%d, %s %s, %s\n", user.ID, user.FirstName, user.LastName, user.Email))
+		}
+	}
+	println(time.Since(now).String())
 }
