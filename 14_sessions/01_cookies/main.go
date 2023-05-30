b1813525484ea67bba2ac25ed2f22909f8744a85
Valeria Tisch <valeria.tisch97@gmail.com>
2023-05-30 20:56:42 +0200

    refactor

--- 14_sessions/01_cookies/main.go
+++ 14_sessions/01_cookies/main.go
@@ -1,60 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/setGeneral", handleSetCookie)
-	http.HandleFunc("/", handleMyCookie)
-	http.HandleFunc("/set", handleSetMyCookie)
-	http.HandleFunc("/read", handleReadMyCookie)
+	http.HandleFunc("/", handleHome)
 
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handleSetCookie(w http.ResponseWriter, req *http.Request) {
-	// Set a "general" cookie with some value
-
-	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
-}
-
-func handleSetMyCookie(w http.ResponseWriter, req *http.Request) {
-	// Set the "my-cookie" cookie with an initial value of 1
-
-	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
-}
-
-func handleMyCookie(w http.ResponseWriter, req *http.Request) {
-	// TODO:
-	// Check if my-cookie exists
-
-	// If the cookie doesn't exist, redirect the user to the "/set" path
-
-	// If the cookie exists, extract its value
-
-	// Update the cookie with the new count
-
-	// Display the visit count to the user
+func handleHome(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func handleReadMyCookie(w http.ResponseWriter, req *http.Request) {
-	// Read and display the value of the "my-cookie" cookie
-	c, err := req.Cookie("my-cookie")
-	if err != nil {
-		log.Println(err)
-	} else {
-		fmt.Fprintln(w, "YOUR COOKIE #1:", c)
-	}
-
-	// Read and display the value of the "general" cookie
-	c1, err := req.Cookie("general")
-	if err != nil {
-		log.Println(err)
-	} else {
-		fmt.Fprintln(w, "YOUR COOKIE #2:", c1)
-	}
-}
+// Goal:
+// 1. Create a cookie with a unique ID for each user & store it in a map (as key)
+// 2. Create a cookie that counts how often the user visits the page and store the count in a map (as value)
+// 3. Create a route /user which will display the UUID and the count for the user
--- 14_sessions/02_login/main.go
+++ 14_sessions/02_login/main.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
-// TODO:
-// 1. Find a partner & try to understand the code
-// 2. Partner A should read & comment the functions createUserDB(), indexHandler(), signupHandler()
-// 3. Partner B should read & comment loginHandler(), homeHandler(), logoutHandler() 
-// 4. Explain the code to each other
-
 var (
 	templates *template.Template
 	users     = make(map[string]string)
 	sessions  = make(map[string]Session)
-	counts    = make(map[string]int)
 )
 
 type User struct {
@@ -32,9 +23,8 @@ type User struct {
 }
 
 type Session struct {
-	Username     string
-	SessionID    string
-	CreationTime time.Time
+	Username  string
+	SessionID string
 }
 
 func init() {
@@ -42,8 +32,6 @@ func init() {
 }
 
 func main() {
-	createUserDB()
-
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/signup", signupHandler)
 	http.HandleFunc("/login", loginHandler)
@@ -55,16 +43,12 @@ func main() {
 }
 
 // Handler Functions
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	// Check if the user is authenticated
 	if isAuthenticated(r) {
-		// Redirect to the home page
 		http.Redirect(w, r, "/home", http.StatusSeeOther)
 		return
 	}
 
-	// Render the index page with sign up and login buttons
 	renderTemplate(w, "index.gohtml", nil)
 }
 
@@ -87,22 +71,6 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 		users[username] = string(hashedPassword)
 
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
-
-		file, err := os.OpenFile("user.csv", os.O_WRONLY|os.O_APPEND, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		writer := csv.NewWriter(file)
-		record := []string{username, string(hashedPassword)}
-		writer.Write(record)
-		writer.Flush()
-
-		if err := writer.Error(); err != nil {
-			log.Fatal(err)
-		}
-		return
 	}
 
 	renderTemplate(w, "signup.gohtml", nil)
@@ -128,12 +96,9 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		sessionID := uuid.New().String()
 
 		sessions[sessionID] = Session{
-			Username:     username,
-			SessionID:    sessionID,
-			CreationTime: time.Now(),
+			Username:  username,
+			SessionID: sessionID,
 		}
-
-		counts[username]++
 
 		setSessionCookie(w, sessionID)
 
@@ -151,34 +116,9 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sessionID := getSessionID(r)
+	session := sessions[sessionID]
 
-	session, ok := sessions[sessionID]
-	if !ok {
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-		return
-	}
-
-	if sessionHasExpired(session) {
-		delete(sessions, sessionID)
-		clearSessionCookie(w)
-
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-		return
-	}
-
-	username := session.Username
-	count := counts[username]
-	counts[username]++
-
-	context := struct {
-		Username string
-		Count    int
-	}{
-		Username: username,
-		Count:    count,
-	}
-
-	renderTemplate(w, "home.gohtml", context)
+	renderTemplate(w, "home.gohtml", session.Username)
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
@@ -188,17 +128,15 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 
 	clearSessionCookie(w)
 
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 // Helper Functions
-
 func setSessionCookie(w http.ResponseWriter, sessionID string) {
 	cookie := http.Cookie{
-		Name:    "session",
-		Value:   sessionID,
-		Expires: time.Now().Add(5 * time.Minute),
-		Path:    "/",
+		Name:  "session",
+		Value: sessionID,
+		Path:  "/",
 	}
 	http.SetCookie(w, &cookie)
 }
@@ -221,55 +159,11 @@ func clearSessionCookie(w http.ResponseWriter) {
 	http.SetCookie(w, &cookie)
 }
 
-func sessionHasExpired(session Session) bool {
-	duration := time.Since(session.CreationTime)
-
-	return duration.Minutes() >= 5
-}
-
 func isAuthenticated(r *http.Request) bool {
 	sessionID := getSessionID(r)
 
 	_, ok := sessions[sessionID]
 	return ok
-}
-
-func createUserDB() {
-	file, err := os.OpenFile("user.csv", os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-
-	records, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if len(records) == 0 {
-		records = append(records, []string{"username", "password_hash"})
-
-		writer := csv.NewWriter(file)
-		writer.WriteAll(records)
-		writer.Flush()
-
-		if err := writer.Error(); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	for _, record := range records[1:] {
-		username := record[0]
-		passwordHash := record[1]
-
-		users[username] = passwordHash
-	}
-
-	for username, passwordHash := range users {
-		fmt.Printf("Username: %s, Password Hash: %s\n", username, passwordHash)
-	}
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
